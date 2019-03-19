930b87e9d12bcdc0c20863e571d95dd18e502fdc
Oliver Broadhurst <Oliver@explore-datascience.net>
2019-03-19 10:37:16 +0200

    Redist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hack-package',
-    version='0.8',
+    version='0.9',
     packages=find_packages(exclude=['test*']),
     license='MIT',
     description='EDSA Hackathon recursion and sorting package',
