a5c88e802a9076d9b7fd968ca6ec627469162749
Oliver Broadhurst <Oliver@explore-datascience.net>
2019-03-19 10:25:15 +0200

    Bug fix

--- hackpackage/recursion.py
+++ hackpackage/recursion.py
@@ -1,3 +1,4 @@
+import numpy as np
 def sum_array(array):
 
     '''Return sum of all items in array'''
@@ -24,6 +25,8 @@ def factorial(n):
     '''Return n!'''
     if n <= 1:
         return n
+    if n == 0:
+        return 1
     else:
         return n * factorial(n-1)
 
